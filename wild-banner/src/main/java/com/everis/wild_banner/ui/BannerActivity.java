4f8e73573a821b09db8b63a752468b751426eeeb
cmedranv <MelharmonY.2>
2022-03-08 11:39:53 -0500

    Initial commit

---
+++ wild-banner/src/main/java/com/everis/wild_banner/ui/BannerActivity.java
@@ -0,0 +1,15 @@
+package com.everis.wild_banner.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.everis.wild_banner.R;
+
+public class BannerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_banner);
+    }
+}
